fee50a893f2b58ca185bd286af99fd76995fa701
Henry Bogaeus <bogaeus@kth.se>
2017-11-05 16:20:46 +0100

    Finished Morse decoding

---
+++ seminars/morse/src/morse.ex
@@ -0,0 +1,56 @@
+defmodule Morse do
+  def base, do: '.- .-.. .-.. ..-- -.-- --- ..- .-. ..-- -... .- ... . ..-- .- .-. . ..-- -... . .-.. --- -. --. ..-- - --- ..-- ..- ... '
+
+  def rolled, do: '.... - - .--. ... ---... .----- .----- .-- .-- .-- .-.-.- -.-- --- ..- - ..- -... . .-.-.- -.-. --- -- .----- .-- .- - -.-. .... ..--.. ...- .----. -.. .--.-- ..... .---- .-- ....- .-- ----. .--.-- ..... --... --. .--.-- ..... ---.. -.-. .--.-- ..... .---- '
+
+  def decode(signal) do
+    table = decode_table()
+    decode(signal, table, table)
+  end
+
+  def decode([], _, _), do: []
+  def decode([?-  | signal], {:node, _, long, _}, table), do: decode(signal, long, table)
+  def decode([?.  | signal], {:node, _, _, short}, table), do: decode(signal, short, table)
+  def decode([?\s | signal], {:node, :na, _, _}, table), do: decode(signal, table, table)
+  def decode([_ | signal], {:node, char, _, _}, table), do: [char | decode(signal, table, table)]
+
+  def decode_table do
+    {:node, :na,
+      {:node, 116,
+        {:node, 109,
+          {:node, 111,
+            {:node, :na, {:node, 48, nil, nil}, {:node, 57, nil, nil}},
+            {:node, :na, nil, {:node, 56, nil, {:node, 58, nil, nil}}}},
+          {:node, 103,
+            {:node, 113, nil, nil},
+            {:node, 122,
+              {:node, :na, {:node, 44, nil, nil}, nil},
+              {:node, 55, nil, nil}}}},
+        {:node, 110,
+          {:node, 107, {:node, 121, nil, nil}, {:node, 99, nil, nil}},
+          {:node, 100,
+            {:node, 120, nil, nil},
+            {:node, 98, nil, {:node, 54, {:node, 45, nil, nil}, nil}}}}},
+      {:node, 101,
+        {:node, 97,
+          {:node, 119,
+            {:node, 106,
+              {:node, 49, {:node, 47, nil, nil}, {:node, 61, nil, nil}},
+              nil},
+            {:node, 112,
+              {:node, :na, {:node, 37, nil, nil}, {:node, 64, nil, nil}},
+              nil}},
+          {:node, 114,
+            {:node, :na, nil, {:node, :na, {:node, 46, nil, nil}, nil}},
+            {:node, 108, nil, nil}}},
+        {:node, 105,
+          {:node, 117,
+            {:node, 32,
+              {:node, 50, nil, nil},
+              {:node, :na, nil, {:node, 63, nil, nil}}},
+            {:node, 102, nil, nil}},
+          {:node, 115,
+            {:node, 118, {:node, 51, nil, nil}, nil},
+            {:node, 104, {:node, 52, nil, nil}, {:node, 53, nil, nil}}}}}}
+  end
+end
