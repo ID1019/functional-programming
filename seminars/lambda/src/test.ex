4646d3256350357a4ee122219048920bd32f9dfc
Johan M <johanmon@kth.se>
2017-11-06 21:46:10 +0100

    an introduction to lambda calculus

---
+++ seminars/lambda/src/test.ex
@@ -0,0 +1,33 @@
+defmodule Test do
+
+  ## Append the hard way
+
+  def append(a, b) do
+    app = fn(x, y, f) -> 
+      case x do
+        [] -> y
+        [h|t] -> [h | f.(t,y,f) ]
+      end
+    end
+    app.(a, b, app)
+  end
+  
+  ## Fibonacci 
+
+  def fib(n) do
+    fib = fn(x,f) -> 
+      case x do
+	0 -> 0
+	1 -> 1
+	d ->
+	  f.(d-1,f) + f.(d-2,f)
+      end
+    end
+    fib.(n, fib)
+  end
+
+
+  
+
+
+end
