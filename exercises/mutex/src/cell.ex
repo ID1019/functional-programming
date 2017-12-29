56da2e7ad9b58f050189eb8360667bb101207357
Simone Stefani <s.stefani95@gmail.com>
2017-12-29 18:50:39 +0100

    Add mutex code

---
+++ exercises/mutex/src/cell.ex
@@ -0,0 +1,57 @@
+defmodule Cell do
+
+  def new, do: spawn_link(fn -> cell(:open) end)
+
+  # Naïve lock
+  defp cell(state) do
+    receive do
+      {:get, from} ->
+        send from, {:ok, state}
+        cell(state)
+      {:set, value, from} ->
+        send from, :ok
+        cell(value)
+    end
+  end
+
+  def get(cell) do
+    send cell, {:get, self()}
+    receive do
+      {:ok, value} -> value
+    end
+  end
+
+  def set(cell, value) do
+    send cell, {:set, value, self()}
+    receive do
+      :ok -> :ok
+    end
+  end
+
+  # Lock with atomic swap
+  defp cell(state) do
+    receive do
+      {:swap, value, from} ->
+        send from, {:ok, state}
+        cell(value)
+      {:set, value, from} ->
+        send from, :ok
+        cell(value)
+    end
+  end
+
+  def swap(cell, value) do
+    send cell, {:swap, value, self()}
+    receive do
+      {:ok, value} -> value
+    end
+  end
+
+  def set(cell, value) do
+    send cell, {:set, value, self()}
+    receive do
+      :ok -> :ok
+    end
+  end
+
+end
---
+++ exercises/mutex/src/semaphore.ex
@@ -0,0 +1,58 @@
+defmodule Semaphore do
+
+  def semaphore(0) do
+    receive do
+      :release ->
+        semaphore(1)
+    end
+  end
+
+  def semaphore(n) do
+    receive do
+      {:request, from} ->
+        send from, :granted
+        semaphore(n - 1)
+      :release ->
+        semaphore(n + 1)
+    end
+  end
+
+  def request(semaphore) do
+    send semaphore, {:request, self()}
+    receive do
+      :granted ->
+        :ok
+    end
+  end
+
+  # Remove deadlock
+  def request(semaphore) do
+    send semaphore, {:request, self()}
+    receive do
+      :granted ->
+        :ok
+    after 1000 ->
+       :abort
+    end
+  end
+
+  # Return resource before getting it
+  def request(semaphore) do
+    ref = make_ref()
+    send semaphore, {:request, ref, self()}
+    wait(semaphore, ref)
+  end
+
+  def wait(semaphore, ref) do
+    receive do
+      {:granted, ^ref} ->
+        :ok
+      {:granted, _} ->
+        wait(semaphore, ref)
+    after 1000 ->
+      send semaphore, :release
+      :abort
+    end
+  end
+
+end
