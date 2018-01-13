4950e58f1c70a0aae5ccfcebfbab3ba6f54bc1fe
Simone Stefani <s.stefani95@gmail.com>
2018-01-14 00:24:36 +0100

    Cleanup of cell

--- exercises/cell/src/cell.ex
+++ exercises/cell/src/cell.ex
@@ -1,31 +1,33 @@
 defmodule Cell do
 
-  ## Create a new cell process
-  def start(n) do
-    spawn fn -> cell(n) end
-  end
+  # Create a new memory cell process.
+  def start(n), do: spawn(fn -> cell(n) end)
 
-  ## Set the internal value of a given cell
+  # Set the internal value of a given cell.
   def set(pid, value) do
-    send pid, {:set, value}
+    send(pid, {:set, value})
     :ok
   end
 
-  ## Get the internal value of a given cell
+  # Get the internal value of a given cell.
   def get(pid) do
-    send pid, {:get, self()}
+    send(pid, {:get, self()})
+
     receive do
       {:ok, value} ->
         {:ok, value}
     end
   end
 
+  # Get the internal value of a given cell with an
+  # asynchronous approach.
   def get_asyn(pid) do
     ref = make_ref()
-    send pid, {:get, ref, self()}
+    send(pid, {:get, ref, self()})
     ref
   end
 
+  # Retrieve value after asynchronous approach.
   def get_answ(ref) do
     receive do
       {:ok, ref, value} ->
@@ -33,17 +35,19 @@ defmodule Cell do
     end
   end
 
-  def cell(n) do
+  # Representation of a memory cell as a process.
+  defp cell(n) do
     receive do
       {:set, value} ->
         cell(value)
+
       {:get, pid} ->
-        send pid, {:ok, n}
+        send(pid, {:ok, n})
         cell(n)
+
       {:get, ref, pid} ->
-        send pid, {:ok, ref, n}
+        send(pid, {:ok, ref, n})
         cell(n)
     end
   end
-
 end
