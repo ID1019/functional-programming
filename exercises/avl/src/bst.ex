9ccd4f24575deb757076dd2019f79ca054e5fe2d
Simone Stefani <s.stefani95@gmail.com>
2018-01-13 21:37:41 +0100

    Cleanup avl

--- exercises/avl/src/bst.ex
+++ exercises/avl/src/bst.ex
@@ -1,9 +1,16 @@
-defmodule Bst do
+defmodule BST do
 
+  # ================================================== #
+  # == Binary Search Tree - Public API == #
+  # ================================================== #
+
+  # Create an empty tree.
   def tree(), do: nil
 
+  # Insert a key-value pair in a tree.
+  # Usage: insert(tree, key, value)
   def insert(nil, key, value) do
-    {:node, key, value, nil, nil} ## empty tree
+    {:node, key, value, nil, nil}
   end
   def insert({:node, key, _, a, b}, key, value) do
     {:node, key, value, a, b}
@@ -15,6 +22,8 @@ defmodule Bst do
     {:node, rk, rv, a, insert(b, kk, kv)}
   end
 
+  # Lookup for a given key in a tree.
+  # Usage: lookup(tree, key)
   def lookup(nil, _), do: :fail
   def lookup({:node, k, v, _, _}, k), do: {:ok, v}
   def lookup({:node, k, _, l, r}, key) do
@@ -24,15 +33,12 @@ defmodule Bst do
     end
   end
 
-  def traverse(nil), do: {0, 0}
-  def traverse({:node, _, _, l, r}) do
-    {lt, lm} = traverse(l)
-    {rt, rm} = traverse(r)    
-    {lt + rt + 1, max(lm, rm) + 1}
-  end
-
+  # Compute the depth of a the leaf containing a given key
+  # in a tree.
+  # Usage: depth(tree, key)
   def depth(nil, _), do: :fail
   def depth({:node, k, _, _, _}, k), do: {:ok, 1}
+
   def depth({:node, k, _, l, r}, key) do
     cond do
       key < k ->
@@ -40,6 +46,7 @@ defmodule Bst do
           {:ok, d} -> {:ok, d + 1}
           :fail -> :fail
         end
+
       true ->
         case depth(r, key) do
           {:ok, d} -> {:ok, d + 1}
@@ -48,9 +55,20 @@ defmodule Bst do
     end
   end
 
+  # Compute the maximum depth of a tree.
+  # Usage: max_depth(tree)
   def max_depth(nil), do: 0
   def max_depth({:node, _, _, l, r}) do
     max(max_depth(l), max_depth(r)) + 1
   end
-    
+
+  # Traverse the tree to compute a tuple containing the 
+  # number of nodes and the maximum depth.
+  # Usage: traverse(tree)
+  def traverse(nil), do: {0, 0}
+  def traverse({:node, _, _, l, r}) do
+    {lt, lm} = traverse(l)
+    {rt, rm} = traverse(r)
+    {lt + rt + 1, max(lm, rm) + 1}
+  end
 end
