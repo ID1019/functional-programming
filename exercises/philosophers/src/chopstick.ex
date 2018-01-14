4fbfbbf5e0b5f08608cd6a91ccb17af6b312990e
Simone Stefani <s.stefani95@gmail.com>
2018-01-14 13:33:57 +0100

    Cleanup philosophers assignment

--- exercises/philosophers/src/chopstick.ex
+++ exercises/philosophers/src/chopstick.ex
@@ -5,88 +5,89 @@ defmodule Chopstick do
     {:stick, stick}
   end
 
-
-  ## The synchronous version of requesting a chopstick.
+  # The synchronous version of requesting a chopstick.
   def request({:stick, pid}) do
-    send pid, {:request, self()}
+    send(pid, {:request, self()})
+
     receive do
       :granted -> :ok
     end
   end
 
   def return({:stick, pid}) do
-    send pid, :return
+    send(pid, :return)
   end
 
-
-  ## Using a timeout to detect deeadlock, does it work?
+  # Using a timeout to detect deadlock, does it work?
   def request({:stick, pid}, timeout) do
-    send pid, {:request, self()}
+    send(pid, {:request, self()})
+
     receive do
-      :granted -> 
+      :granted ->
         :ok
-    after timeout -> 
-      :no
+    after
+      timeout ->
+        :no
     end
   end
 
-
-  ## The better version, we keep track of requests.
+  # The better version, we keep track of requests.
   def request({:stick, pid}, ref, timeout) do
-    send pid, {:request, ref, self()}
+    send(pid, {:request, ref, self()})
     wait(ref, timeout)
   end
 
-  def wait(ref, timeout) do
+  defp wait(ref, timeout) do
     receive do
       {:granted, ^ref} ->
         :ok
+
       {:granted, _} ->
         wait(ref, timeout)
-    after timeout ->
-      :no
+    after
+      timeout ->
+        :no
     end
   end
 
-  def return({:stick, pid}, ref) do
-    send pid, {:return, ref}
+  defp return({:stick, pid}, ref) do
+    send(pid, {:return, ref})
   end
 
-
-  ## A asynchronous request, divided into sending the request and
-  ## waiting for the reply.
-  def asynch({:stick, pid}, ref) do
-    send pid, {:request, ref, self()}
+  # A asynchronous request, divided into sending the
+  # request and waiting for the reply.
+  defp asynch({:stick, pid}, ref) do
+    send(pid, {:request, ref, self()})
   end
 
-
-  ##To terminate the process.
+  # To terminate the process.
   def quit({:stick, pid}) do
-    send pid, :quit
+    send(pid, :quit)
   end
 
+  # Initalizing the chopstick.
+  defp init(), do: available()
 
-  ##Initalizing the chopstick.
-  def init(), do: available()
-
-  ## The two states of the chopstick.
-  def available() do
+  # The two states of the chopstick.
+  defp available() do
     receive do
       {:request, from} ->
-        send from, :granted
+        send(from, :granted)
         gone()
+
       :quit ->
         :ok
     end
   end
 
-  def gone() do
+  defp gone() do
     receive do
       :return ->
         available()
+
       :quit ->
         :ok
     end
   end
-  
+
 end
