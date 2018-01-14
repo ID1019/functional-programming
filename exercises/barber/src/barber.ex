cfad8ccfcedb10973d06aca538b4a77d56a7c700
Simone Stefani <s.stefani95@gmail.com>
2018-01-14 10:17:06 +0100

    Cleanup barber

--- exercises/barber/src/barber.ex
+++ exercises/barber/src/barber.ex
@@ -1,24 +1,31 @@
 defmodule Barber do
   
+  # The time needed for an haircut.
   @cut 1000
 
+  # Start a barber process.
   def start(waiting) do
     {:ok, spawn_link(fn -> init(waiting) end)}
   end
 
   defp init(waiting), do: barber(waiting)
-  
+
+  # If the barber is free it notifies the waiting system
+  # to send a new customer. Then invites the customer to
+  # have a seat and gives the haircut. Gently stop the
+  # process when the shop closes.
   defp barber(waiting) do
-    send waiting, {:next, self()}
+    send(waiting, {:next, self()})
+
     receive do
       :close ->
         :ok
+
       {:ok, customer} ->
-        send customer, :have_a_seat
+        send(customer, :have_a_seat)
         :timer.sleep(@cut)
-        send customer, :howz_that
+        send(customer, :howz_that)
         barber(waiting)
     end
   end
-  
 end
--- exercises/barber/src/customer.ex
+++ exercises/barber/src/customer.ex
@@ -1,7 +1,8 @@
 defmodule Customer do
-
+  
   @walk 1000
 
+  # Start a customer process.
   def start(name, shop) do
     {:ok, spawn_link(fn -> init(name, shop) end)}
   end
@@ -11,25 +12,36 @@ defmodule Customer do
     customer(name, shop)
   end
 
+  # When a customer enters the shop three different
+  # scenarios can happen:
+  # 1) the barber is free and will give the haircut
+  # 2) the barber is busy but there is space in the waiting
+  #    queue; the customer is asked to wait
+  # 3) the barber is busy and there is no space in the
+  #    waiting queue; the customer is invited to take a
+  #    walk and return later
   defp customer(name, shop) do
     IO.puts("#{name} - enter the shop")
-    send shop, {:hello, self()}
+    send(shop, {:hello, self()})
+
     receive do
       :please_wait ->
         IO.puts("#{name} - waiting")
+
         receive do
           :have_a_seat ->
             IO.puts("#{name} - taking a seat")
+
             receive do
               :howz_that ->
                 IO.puts("#{name} - thank you")
             end
         end
+
       :sorry ->
         IO.puts("#{name} - taking a walk in the sun")
         :timer.sleep(@walk)
         customer(name, shop)
     end
   end
-  
 end
