8cbd786d68c19e09c56642e107039aa18e107487
Johan Montelius <johanmon@kth.se>
2018-02-13 17:52:19 +0100

    asynchronous done

---
+++ lectures/asynchronous/src/memory.ex
@@ -0,0 +1,22 @@
+defmodule Memory do
+
+  def new(list) do
+    cells = Enum.map(list, fn(e) -> Cell.start(e) end)
+    {:mem, List.to_tuple(cells)}
+  end
+
+  def read({:mem, mem}, n) do
+    cell = elem(mem, n)
+    Cell.read(cell)
+  end
+
+  def write({:mem, mem}, n, val) do
+    cell = elem(mem, n)
+    Cell.write(cell, val)
+  end
+
+  def delete({:mem, mem}) do
+    Enum.each(Tuple.to_list(mem), fn(c) -> send(c, :quit) end)
+  end
+    
+end
