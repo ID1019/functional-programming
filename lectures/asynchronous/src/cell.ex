949efea7c5b48631b80caf5b90bb9f037f3e0a06
Johan Montelius <johanmon@kth.se>
2018-02-13 17:52:19 +0100

    asynchronous done

---
+++ lectures/asynchronous/src/cell.ex
@@ -0,0 +1,39 @@
+defmodule Cell do
+
+  def start(val) do       ## things to do in mother process
+    {:cell, spawn_link(fn() -> init(val) end)}
+  end
+
+  def read({:cell, cell}) do
+    send(cell , {:read, self()})
+    receive do
+      {:value, v} ->
+	v
+    end
+  end
+
+  def write({:cell, cell}, val) do
+    send(cell, {:write, val, self()})
+    receive do
+    :ok ->
+      :ok
+    end
+  end
+
+  def init(val) do        ## things to do in the child process
+    cell(val)
+  end
+
+  def cell(v) do
+    receive do
+      {:read, pid} ->
+	send(pid, {:value, v})
+	cell(v)
+      {:write, w, pid} ->
+	send(pid, :ok)
+	cell(w)
+    end
+  end
+
+end
+  
