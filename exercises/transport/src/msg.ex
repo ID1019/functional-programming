42cad7d7c5a75311498f722a8fc097ccff2d7460
Johan Montelius <johanmon@kth.se>
2018-02-14 14:18:53 +0100

    link, network, order and flow examples running, structs used for messages, switch still to do

--- exercises/transport/src/msg.ex
+++ exercises/transport/src/msg.ex
@@ -2,9 +2,9 @@ defmodule Msg do
 
   defstruct data: nil
 
-  defimpl String.Chars, for: Dgr do
+  defimpl String.Chars, for: Msg do
     def to_string(%Msg{data: data})  do
-      "<Msg: data: " <> String.Chars.to_string(data) <> ">"
+      "Msg<data: " <> String.Chars.to_string(data) <> ">"
     end
   end
 
