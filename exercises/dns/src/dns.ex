00ad6efc868fca4a7fb4cf66b5e0c407d1d6f2da
Johan M <johanmon@kth.se>
2018-03-05 08:46:14 +0100

    some last minute rewrites :-)

--- exercises/dns/src/dns.ex
+++ exercises/dns/src/dns.ex
@@ -1,67 +1,66 @@
 defmodule DNS do
 
-  @server "8.8.8.8"
+  @server {8,8,8,8}
   @port 53
   @local 5300
-  @timeout 4000
-
-  def start, do: start(@local, @server)
-  def start(port), do: start(port, @server)
-  def start(port, server) do
-    spawn_link(fn -> init(port, server) end)
+  
+  def start() do
+    start(@local, @server, @port)
   end
 
-  def init(port, server) do
-    case :gen_udp.open(port, [{:active, true}, :binary]) do
-      {:ok, socket} ->
-        dns(socket, server)
+  def start(local, server, port) do
+    spawn(fn() -> init(local, server, port) end)
+  end
 
+  def init(local, server, port) do
+    case :gen_udp.open(local, [{:active, true}, :binary]) do
+      {:ok, local} ->
+	case :gen_udp.open(0, [{:active, true}, :binary]) do
+	  {:ok, remote} ->
+            dns(local, remote, server, port, 0, Map.new())
+	  error ->
+            :io.format("DNS error opening remote socket: ~w~n", [error])
+	end
       error ->
-        :io.format("DNS error opening server socket: ~w~n", [error])
+        :io.format("DNS error opening local socket: ~w~n", [error])
     end
   end
 
-  def dns(socket, server) do
+  def dns(local, remote, server, server_port, n, queries) do
+
     receive do
-      {:udp, socket, ip, port, packet} ->
-        reply = fn(rep) -> :gen_udp.send(socket, ip, port, rep) end
-        frw = fn(req) -> forward(req, server) end
-        Handler.start(packet, reply, frw)
-        dns(socket, server)
+      {:udp, ^local, client, client_port, msg} ->
+	:io.format("message from  ~w:~w~n", [client, client_port])
+	{id, _qr, _op, _aa, _tc, _rd, _ra, _resp, body} = Msg.decode(msg)
+	:io.format("id: ~w, body: ~p~n", [id, body])
+	query = Msg.fake(msg, n)
+	:gen_udp.send(remote, server, server_port, query)
+	dns(local, remote, server, server_port, n+1, Map.put(queries, n, {id, client, client_port}))
 
-      {:update} ->
-        dns(socket, server)
+      {:udp, ^remote, server, server_port, msg} ->
+	:io.format("reply from  ~w:~w~n", [server, server_port])
+	{id, _qr, _op, _aa, _tc, _rd, _ra, _resp, reply} = Msg.decode(msg)
+	:io.format("id: ~w, reply: ~p~n", [id, reply])
+	{{id, client, client_port}, rest} = Map.pop(queries, id)
+	response = Msg.fake(msg, id)
+	:gen_udp.send(local, client, client_port, response)
+	dns(local, remote, server, server_port, n, rest)	
 
-      {:stop} ->
-        :io.format("Bye, bye!")
-        :ok
+      :update ->
+	DNS.dns(local, remote, server, server_port, n, queries)
 
-      error ->
-        :io.format("Strange message: ~w~n", [error])
-        dns(socket, server)
+      :status ->
+	:io.format("local: ~w~nremote: ~w~nserver ~w~nport ~w~n", [local, remote, server, server_port])
+	dns(local, remote, server, server_port, n, queries)
+
+      :quit ->
+	:ok
+
+      strange ->
+	:io.format("strange: ~w~n", [strange])
+	dns(local, remote, server, server_port, n, queries)
     end
-  end
+  end    
 
-  def forward(request, server) do
-    case :gen_udp.open(0, [{:active, true}, :binary]) do
-      {:ok, client} ->
-        :gen_udp.send(client, server, @port, request)
-
-        result =
-          receive do
-            {:udp, ^client, _ip, _port, reply} ->
-              {:ok, reply}
-          after
-            @timeout ->
-              {:error, :timeout}
-          end
-
-        :gen_udp.close(client)
-        result
-
-      error ->
-        {:error, error}
-    end
-  end
-
+  
 end
--- exercises/dns/src/msg.ex
+++ exercises/dns/src/msg.ex
@@ -1,6 +1,6 @@
 defmodule Msg do
 
-  ## The most common records
+  ## The most common records types
   @a 1
   @ns 2
   @cname 5
@@ -10,102 +10,102 @@ defmodule Msg do
   @txt 16
   @aaaa 28
 
+  ## The only class we care about
+
+  @int 1
+
   def decode(<<id::16, flags::binary-size(2), qdc::16, anc::16, nsc::16, arc::16, body::binary>> = raw) do
     <<qr::1, op::4, aa::1, tc::1, rd::1, ra::1, _::3, resp::4>> = flags
-    decoded = decode_body(qdc, anc, nsc, arc, body, raw)
-    {id, qr, op, aa, tc, rd, ra, resp, decoded}
+    body = decode_body(qdc, anc, nsc, arc, body, raw)
+    {id, qr, op, aa, tc, rd, ra, resp, body}
   end
 
-  def decode_body(qdc, anc, nsc, arc, body0, raw) do
-    {query, body1} = decode_query(qdc, body0, raw)
-    {answer, body2} = decode_answer(anc, body1, raw)
-    {authority, body3} = decode_answer(nsc, body2, raw)
-    {additional, _} = decode_answer(arc, body3, raw)
+  def fake(<<_::16, rest::binary>>, id) do
+    <<id::16, rest::binary>>
+  end
+  
+  
+  def decode_body(qdc, anc, nsc, arc, body, raw) do
+    {query, rest} = decode_query(qdc, body, raw)
+    {answer, rest} = decode_answer(anc, rest, raw)
+    {authority, rest} = decode_answer(nsc, rest, raw)
+    {additional, _} = decode_answer(arc, rest, raw)
     {query, answer, authority, additional}
   end
 
-  defp decode_query(0, body, _), do: {[], body}
-  defp decode_query(n, queries, raw) do
+  def decode_query(0, body, _), do: {[], body}
+  def decode_query(n, queries, raw) do
     {name, <<qtype::16, qclass::16, next::binary>>} = decode_name(queries, raw)
     {decoded, body} = decode_query(n - 1, next, raw)
     {[{name, qtype, qclass} | decoded], body}
   end
 
-  defp decode_answer(0, body, _), do: {[], body}
-  defp decode_answer(n, answers, raw) do
-    {name, <<type::16, class::16, ttl::32, rdlength::16, rest::binary>>} =
-      decode_name(answers, raw)
-
+  def decode_answer(0, body, _), do: {[], body}
+  def decode_answer(n, answers, raw) do
+    {name, <<type::16, class::16, ttl::32, rdlength::16, rest::binary>>} = decode_name(answers, raw)
     {rdata, next} = decode_rdata(rdlength, rest)
     record = decode_record(type, class, rdata, raw)
     {decoded, body} = decode_answer(n - 1, next, raw)
-    {[{name, ttl, record} | decoded], body}
+    {[{name,  ttl, record} | decoded], body}
   end
 
-  defp decode_rdata(n, rest) do
+  def decode_rdata(n, rest) do
     <<rdata::binary-size(n), next::binary>> = rest
     {rdata, next}
   end
 
-  defp decode_record(@a, _, <<i1::8, i2::8, i3::8, i4::8>>, _) do
-    {:a, {i1, i2, i3, i4}}
+  def decode_record(@a, @int, <<i1::8, i2::8, i3::8, i4::8>>, _) do
+    {:a, :int, {i1, i2, i3, i4}}
   end
-  defp decode_record(@ns, _, rdata, raw) do
+  def decode_record(@ns, @int, rdata, raw) do
     {name, _} = decode_name(rdata, raw)
-    {:ns, name}
+    {:ns, :int, name}
   end
-  defp decode_record(@cname, _, rdata, raw) do
+  def decode_record(@cname, @int, rdata, raw) do
     {name, _} = decode_name(rdata, raw)
-    {:cname, name}
+    {:cname, :int, name}
   end
-  defp decode_record(@soa, _, rdata, raw) do
+  def decode_record(@soa, @int, rdata, raw) do
     {primary, rd1} = decode_name(rdata, raw)
     {admin, rd2} = decode_name(rd1, raw)
     <<serial::32, refr::32, retr::32, exp::32, ttl::32>> = rd2
-    {:soa, primary, admin, serial, refr, retr, exp, ttl}
+    {:soa, :int, primary, admin, serial, refr, retr, exp, ttl}
   end
-  defp decode_record(@mx, _, <<pred::16, rdata::binary>>, raw) do
+  def decode_record(@mx, @int, <<pred::16, rdata::binary>>, raw) do
     {name, _} = decode_name(rdata, raw)
-    {:mx, pred, name}
+    {:mx, :int, pred, name}
   end
-  defp decode_record(@ptr, _, rdata, raw) do
+  def decode_record(@ptr, @int, rdata, raw) do
     {name, _} = decode_name(rdata, raw)
-    {:ptr, name}
+    {:ptr, :int, name}
   end
-  defp decode_record(@txt, _, rdata, _) do
-    {:txt, :binary.binary_to_list(rdata)}
+  def decode_record(@txt, @int, rdata, _) do
+    {:txt, :int, :binary.binary_to_list(rdata)}
   end
-  defp decode_record(@aaaa, _, rdata, _) do
-    {:ipv6, rdata}
+  def decode_record(@aaaa, @int, rdata, _) do
+    {:ipv6, :int, rdata}
   end
-  defp decode_record(type, class, rdata, _) do
+  def decode_record(type, class, rdata, _) do
     {type, class, rdata}
   end
 
-  def decode_name(label, raw), do: decode_name(label, [], raw)
-  def decode_name(<<0::1, 0::1, _::6, _::binary>> = label, names, raw) do
-    ## regular name encoding
-    decode_label(label, names, raw)
-  end
-  def decode_name(<<1::1, 1::1, n::14, rest::binary>>, names, raw) do
-    ## offset encoding
-    offset = 8 * n
-    <<_::size(offset), section::binary>> = raw
-    {name, _} = decode_label(section, names, raw)
-    {name, rest}
+  def decode_name(label, raw) do
+    decode_names(label, [], raw)
   end
 
-  defp decode_label(<<0::8, rest::binary>>, names, _) do
+  def decode_names(<<0::1, 0::1, 0::6, rest::binary>>, names, _raw) do  
     {Enum.reverse(names), rest}
   end
-  defp decode_label(<<n::8, rest::binary>>, names, raw) do
-    decode_label(n, rest, [], names, raw)
+  def decode_names(<<0::1, 0::1, n::6, _::binary>> = label, names, raw) do
+    ## regular name encoding
+    <<_::8, name::binary-size(n), rest::binary>> = label
+    decode_names(rest, [name|names], raw)
   end
-  defp decode_label(0, rest, sofar, names, raw) do
-    decode_name(rest, [Enum.reverse(sofar) | names], raw)
-  end
-  defp decode_label(n, <<char::8, rest::binary>>, sofar, names, raw) do
-    decode_label(n - 1, rest, [char | sofar], names, raw)
+  def decode_names(<<1::1, 1::1, n::14, rest::binary>>, names, raw) do
+    ## offset encoding
+    <<_::binary-size(n), section::binary>> = raw
+    {name, _} = decode_names(section, names, raw)
+    {name, rest}
   end
 
 end
