228ec4839c97ba2c1455c27f84847a18a5b96c27
Johan Montelius <johanmon@kth.se>
2019-01-23 15:10:27 +0100

    partial derivative of y given x is 0

--- exercises/derivative/src/derivative.ex
+++ exercises/derivative/src/derivative.ex
@@ -23,7 +23,7 @@ defmodule Derivative do
 
   def deriv({:const, _}, _) do {:const, 0} end
   def deriv({:var, v}, v) do {:const, 1} end
-  def deriv({:var, y}, _) do {:var, y} end
+  def deriv({:var, y}, _) do {:const, 0} end
   def deriv({:mul, e1, e2}, v) do
     {:add, {:mul, deriv(e1, v), e2}, {:mul, e1, deriv(e2, v)}}
   end
