d836ae9e5754a34a8e2c0e1c95e06e6fff0fa8f3
Johan M <johanmon@kth.se>
2020-01-30 19:23:55 +0100

    added some code

--- exercises/huffman/src/huffman.ex
+++ exercises/huffman/src/huffman.ex
@@ -16,10 +16,10 @@ defmodule Huffman do
   def test do
     sample = sample()
     tree = tree(sample)
-    encode = encode_table(tree)
+    encode = encode_tuple(tree)
     decode = decode_table(tree)
     text = text()
-    seq = encode(text, encode)
+    seq = encode_tuple(text, encode)
     decode(seq, decode)
   end
 
@@ -29,6 +29,8 @@ defmodule Huffman do
     huffman(freq)
   end
 
+
+  
   # Compute the frequencies of all the characters in the
   # sample text and return a list of tuples {char, freq}.
   def freq(sample), do: freq(sample, [])
@@ -46,12 +48,23 @@ defmodule Huffman do
     [elem | update(char, freq)]
   end
 
+
+
+
+
+
+
+
+
+
+  
   # Build the actual Huffman tree inserting a character at
   # time based on the frequency.
   def huffman(freq) do
     sorted = Enum.sort(freq, fn({_, x}, {_, y}) -> x < y end)
     huffman_tree(sorted)
   end
+  
 
   def huffman_tree([{tree, _}]), do: tree
   def huffman_tree([{a, af}, {b, bf} | rest]) do
@@ -66,10 +79,15 @@ defmodule Huffman do
     [{b, bf} | insert({a, af}, rest)]
   end
 
+
+
+
+  
   # Build the encoding table.
   def encode_table(tree) do
-    codes(tree, [])
+    #codes(tree, [])
     # codes_better(tree, [], [])
+    Enum.sort( codes_better(tree, [], []), fn({_,x},{_,y}) -> length(x) < length(y) end)
   end
 
   # Traverse the Huffman tree and build a binary encoding
@@ -79,10 +97,15 @@ defmodule Huffman do
     bs = codes(b, [1 | sofar])
     as ++ bs
   end
-  def codes(a, code) do
+  def codes( a, code) do
     [{a, Enum.reverse(code)}]
   end
 
+
+
+
+  ## A better travering of the tree
+  
   def codes_better({a, b}, sofar, acc) do
     left = codes_better(a, [0 | sofar], acc)
     codes_better(b, [1 | sofar], left)
@@ -91,6 +114,57 @@ defmodule Huffman do
     [{a, Enum.reverse(code)} | acc]
   end
 
+
+
+  ## An alternative way of representing the encode table.
+
+  def encode_tuple(tree) do
+    codes = codes(tree, [])
+    sorted = Enum.sort(codes, fn({x,_}, {y,_}) ->  x < y end)
+    extended = extend_codes(sorted, 0)
+    List.to_tuple(extended)
+  end
+
+  def extend_codes([], _) do [] end
+  def extend_codes([{n,code}|rest], n) do [code | extend_codes(rest, n+1)] end  
+  def extend_codes(codes, n) do [ [] | extend_codes(codes, n+1)] end  
+  
+  def encode_tuple([], _), do: []
+  def encode_tuple([char | rest], table) do
+     code = elem(table, char)
+     code ++ encode_tuple(rest, table)
+  end
+
+  ## An improvement where we do not waste any stack space
+  
+  #def encode_tuple(text, table) do
+  #   encode_tuple(text, table, [])
+  #end
+  
+  def encode_tuple([], _, acc) do flattenr(acc, []) end
+  
+  def encode_tuple([char | rest], table, acc) do
+    code = elem(table, char)
+    encode_tuple(rest, table, [code | acc])
+  end
+
+  def flattenr([], acc) do acc end
+  def flattenr([code|rest], acc) do
+    # this could further be improved if we didn't reverse the code 
+    flattenr(rest, code ++ acc)
+  end  
+
+  # if code was stored in the reveresed order
+  def add([], acc) do acc end
+  def add([b|rest], acc) do   
+    add(rest, [b|acc])
+  end
+  
+
+
+  
+
+  
   # Parse a string of text and encode it with the
   # previously generated encoding table.
   def encode([], _), do: []
@@ -99,9 +173,16 @@ defmodule Huffman do
     code ++ encode(rest, table)
   end
 
+
+  
+
+
+
+  
   # Decode a string of text using the same encoding
   # table as above. This is a shortcut and an
   # unrealistic situation.
+
   def decode_table(tree), do: codes(tree, [])
 
   def decode([], _), do: []
@@ -121,64 +202,45 @@ defmodule Huffman do
     end
   end
 
+
+
+
+
+
+  
+
   # # The decoder using the tree. This is a more realistic
   # # solution.
-  # def decode_table(tree), do: tree
 
-  # def decode(seq, tree), do: decode(seq, tree, tree)
-  # def decode([], char, _), do: [char]
-  # def decode([0 | seq], {left, _}, tree) do
-  #   decode(seq, left, tree)
-  # end
-  # def decode([1 | seq], {_, right}, tree) do
-  #   decode(seq, right, tree)
-  # end
-  # def decode(seq, char, tree) do
-  #   [char | decode(seq, tree, tree)]
-  # end
+   # def decode_table(tree) do tree end
 
-  # Get a suitable chunk of text to encode.
-  def kallocain(n, coding) do
-    {:ok, file} = File.open("kallocain.txt", [:read])
-    binary = IO.read(file, n)
-    File.close(file)
-
-    length = byte_size(binary)
-    character_decode(binary, length, coding)
+   # def decode(seq, tree) do
+   #   decode(seq, tree, tree)
+   # end
+  
+  def decode([], char, _)  do
+    [char]
+  end
+  
+  def decode([0 | seq], {left, _}, tree) do
+    decode(seq, left, tree)
+  end
+  def decode([1 | seq], {_, right}, tree) do
+    decode(seq, right, tree)
+  end
+  def decode(seq, char, tree) do
+    [char | decode(seq, tree, tree)]
   end
 
-  # Several character encodings are available:
-  # - latin1 will force to read one byte at a time
-  # - utf8 will read the content character by character
-  #   where a character might be two bytes (or more). The
-  #   characters: å, ä and ö are two bytes.
-  # - utf16 will read the content two bytes at a time
-  #   (possibly more).
-  # - utf24 is a faked coding scheme that will simply read
-  #   three bytes at a time.
-  def character_decode(binary, length, :latin1) do
-    {:binary.bin_to_list(binary), length}
-  end
-  def character_decode(binary, length, :utf8) do
-    {:unicode.characters_to_list(binary, :utf8), length}
-  end
-  def character_decode(binary, length, :utf16) do
-    case :unicode.characters_to_list(binary, :utf16) do
-      {:incomplete, list, rest} ->
-        {list, length - byte_size(rest)}
-
-      list ->
-        {list, length}
-    end
-  end
-
+ 
+ 
   # This is the benchmark of the single operations in the
   # Huffman encoding and decoding process.
-  def bench(n, coding) do
-    {sample, _} = kallocain(n, coding)
-    {{text, b}, t1} = time(fn -> kallocain(n, coding) end)
+
+  def bench(file, n) do
+    {text, b} = read(file, n)
     c = length(text)
-    {tree, t2} = time(fn -> tree(sample) end)
+    {tree, t2} = time(fn -> tree(text) end)
     {encode, t3} = time(fn -> encode_table(tree) end)
     s = length(encode)
     {decode, _} = time(fn -> decode_table(tree) end)
@@ -187,7 +249,6 @@ defmodule Huffman do
     r = Float.round(e / b, 3)
     {_, t6} = time(fn -> decode(encoded, decode) end)
 
-    IO.puts("read in #{t1} ms")
     IO.puts("text of #{c} characters")
     IO.puts("tree built in #{t2} ms")
     IO.puts("table of size #{s} in #{t3} ms")
@@ -203,4 +264,50 @@ defmodule Huffman do
     final = Time.utc_now()
     {result, Time.diff(final, initial, :microsecond) / 1000}
   end
+
+ # Get a suitable chunk of text to encode.
+  def read(file, n) do
+   {:ok, fd} = File.open(file, [:read])
+    binary = IO.read(fd, n)
+    File.close(fd)
+
+    length = byte_size(binary)
+    case :unicode.characters_to_list(binary, :utf8) do
+      {:incomplete, chars, rest} ->
+        {chars, length - byte_size(rest)}
+      chars ->
+        {chars, length}
+    end
+  end
+  
+
+
+  def banch(file, n) do
+    {text, b} = read(file, n)
+    c = length(text)
+    {tree, t2} = time(fn -> tree(text) end)
+    {encode, t3} = time(fn -> encode_tuple(tree) end)
+    s = tuple_size(encode)
+    {decode, _} = time(fn -> decode_table(tree) end)
+    {encoded, t5} = time(fn -> encode_tuple(text, encode) end)
+    e = div(length(encoded), 8)
+    r = Float.round(e / b, 3)
+    {_, t6} = time(fn -> decode(encoded, decode) end)
+
+    IO.puts("text of #{c} characters")
+    IO.puts("tree built in #{t2} ms")
+    IO.puts("table of size #{s} in #{t3} ms")
+    IO.puts("encoded in #{t5} ms")
+    IO.puts("decoded in #{t6} ms")
+    IO.puts("source #{b} bytes, encoded #{e} bytes, compression #{r}")
+  end
+
+
+  
+
+  
+
 end
+
+
+
