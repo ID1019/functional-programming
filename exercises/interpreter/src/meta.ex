f76fc0777e7a2ddc4cd02aaac4b834be6c64234a
Johan Montelius <johanmon@kth.se>
2021-02-12 00:03:50 +0100

    adding macros to interpreter

---
+++ exercises/interpreter/src/meta.ex
@@ -0,0 +1,103 @@
+defmodule Meta  do
+
+  ## macor that allows definition of functions
+  
+  defmacro fun(head, do: body), do: do_def(head, body)
+
+  defp do_def({name, hl, parms}, body) do    
+    param = encode_parms(parms)
+    seq = encode_body(body)
+    {{:., [], [{:__aliases__, [alias: false], [:Kernel]}, :def]},
+     [], [{name, hl, []}, [do: {:__block__, [], [{param, seq}]}]]}
+  end
+
+  ## transform before evaluate
+  
+  defmacro eval(body) do
+    seq = encode_body(body)
+    quote do Eager.eval(unquote(seq)) end
+  end
+
+
+
+  ## encoding Elixir AST as Meta AST
+  
+  def encode_body({:__block__, _, body}) do
+    encode_seq(body)
+  end
+  def encode_body(expr) do
+    [encode_expr(expr)]
+  end  
+  
+  def encode_parms(parms), do: Enum.map(parms, fn({var, _, nil}) -> var end)
+
+  def encode_seq([expr]), do: [encode_expr(expr)]
+  def encode_seq([match | seq]) do
+    [encode_match(match) | encode_seq(seq)]
+  end
+  
+  def encode_match({:=, _, [pattern, expr]}) do
+    {:{}, [], [:match, encode_pattern(pattern) , encode_expr(expr)]}
+  end
+
+  def encode_pattern({:_, _, nil}) do
+    :ignore
+  end
+  def encode_pattern({name, _, nil}) do
+    {:{}, [], [:var, name]}
+  end
+  def encode_pattern(atm) when is_atom(atm) do
+    {:{}, [], [:atm, atm]}    
+  end
+  def encode_pattern({p1, p2}) do
+    {:{}, [], [:cons, encode_pattern(p1), encode_pattern(p2)]}
+  end
+  def encode_pattern(pattern) do
+    IO.inspect(pattern)
+    :pattern
+  end      
+
+
+  def encode_expr(atm) when is_atom(atm) do
+    {:{}, [], [:atm, atm]}    
+  end
+  def encode_expr({e1, e2}) do
+    {:{}, [], [:cons, encode_expr(e1), encode_expr(e2)]}
+  end
+  def encode_expr({:fn, _, [ {:->, _, [parms, body]}]}) do
+    {:{}, [], [:lambda, encode_parms(parms), [], encode_body(body)]}
+  end
+  def encode_expr({:case, _, [ expr, [do: clauses]]}) do
+    {:{}, [], [:case, encode_expr(expr), encode_clauses(clauses)]}
+  end
+
+  def encode_expr({{:., _,  [expr]}, _, args}) do
+    {:{}, [], [:apply, encode_expr(expr), encode_args(args)]}
+  end
+
+  def encode_expr({name, _, nil}) do
+    {:{}, [], [:var, name]}  
+  end
+  def encode_expr({name, _, args}) when is_list(args) do
+    {:{}, [], [:apply, {:fun, name}, encode_args(args)]}  
+  end  
+  
+  def encode_expr(expr) do
+    IO.inspect(expr)
+    :expr
+  end      
+
+  def encode_args(args) do
+    Enum.map(args, fn(arg) -> encode_expr(arg) end)
+  end
+  
+  
+  def encode_clauses(clauses) do
+    Enum.map(clauses,
+      fn ({:->, _, [[pattern], body]}) ->
+	{:{}, [], [:clause, encode_pattern(pattern), encode_body(body)]}
+      end)
+  end
+ 
+end
+
--- exercises/interpreter/src/test.ex
+++ exercises/interpreter/src/test.ex
@@ -7,16 +7,14 @@ defmodule Test do
 
   def test(module, 1) do
     seq = [{:atm, :a}]
-    prgm = []
     IO.write("atom: \n    :a  \nshould result in {:ok, :a}\n")
-    apply(module, :eval, [seq, prgm])
+    apply(module, :eval, [seq])
   end
 
   def test(module, 2) do
     seq = [{:match, {:var, :x}, {:atm, :a}}, {:cons, {:var, :x}, {:atm, :b}}]
-    prgm = []
-    IO.write("sequence:\n  x = :a; [x|:b]\nshould result in {:ok, [:a | :b]}\n")
-    apply(module, :eval, [seq, prgm])
+    IO.write("sequence:\n  x = :a; {x,:b}\nshould result in {:ok, {:a,:b}}\n")
+    apply(module, :eval, [seq])
   end
 
   def test(module, 3) do
@@ -26,9 +24,8 @@ defmodule Test do
       {:match, {:cons, :ignore, {:var, :z}}, {:var, :y}},
       {:var, :z}
     ]
-    prgm = []
-    IO.write("sequence:\n x = :a; y = [ x | :b ];  [_ | z] = y; z\n   should result in {:ok, :b}\n")
-    apply(module, :eval, [seq, prgm])
+    IO.write("sequence:\n x = :a; y = {x,:b};  {_,z} = y; z\n   should result in {:ok, :b}\n")
+    apply(module, :eval, [seq])
   end
 
   def test(module, 4) do
@@ -37,23 +34,21 @@ defmodule Test do
       {:case, {:var, :x},
        [{:clause, {:atm, :b}, [{:atm, :ops}]}, {:clause, {:atm, :a}, [{:atm, :yes}]}]}
     ]
-    prgm = []
     IO.write("case expression: \n x = :a; case x do :b -> :ops; :a -> :yes end \nshould result in {:ok, :yes}\n")
-    apply(module, :eval, [seq, prgm])
+    apply(module, :eval, [seq])
   end
 
   def test(module, 5) do
     seq = [
-      {:match, {:var, :x}, {:cons, {:atm, :a}, {:atm, []}}},
+      {:match, {:var, :x}, {:cons, {:atm, :a}, {:atm, :nil}}},
       {:case, {:var, :x},
        [
-         {:clause, {:atm, []}, [{:atm, :ops}]},
+         {:clause, {:atm, :nil}, [{:atm, :ops}]},
          {:clause, {:cons, {:var, :hd}, {:var, :tl}}, [{:var, :hd}]}
        ]}
     ]
-    prgm = []
     IO.write("case expression: \n   x = [a]; case x do [] -> :ops; [hd|tl] -> hd end\n should result in {:ok, :a}\n")
-    apply(module, :eval, [seq, prgm])
+    apply(module, :eval, [seq])
   end
 
   def test(module, 6) do
@@ -62,87 +57,46 @@ defmodule Test do
       {:match, {:var, :f}, {:lambda, [:y], [:x], [{:cons, {:var, :x}, {:var, :y}}]}},
       {:apply, {:var, :f}, [{:atm, :b}]}
     ]
-    prgm = []
-    IO.write("lambda expression: x = :a; f = fn(y) -> [x|y] end; f.(:b)  should result in {:ok, [:a | :b]}\n")
-    apply(module, :eval, [seq, prgm])
+    IO.write("lambda expression: x = :a; f = fn(y) -> {x,y} end; f.(:b)  should result in {:ok, {:a, :b}}\n")
+    apply(module, :eval, [seq])
   end
 
+  ## Requires that append/2 and   are defined in Prgm module 
+  
   def test(module, 7) do
-    seq = [{:call, :append, [{:atm, []}, {:atm, []}]}]
-    prgm = prg()
-    IO.write("function call: \n    append([], []) \n should result in {:ok, []}\n")
-    apply(module, :eval, [seq, prgm])
+    seq = [{:apply, {:fun, :append}, [{:atm, :nil}, {:atm, :nil}]}]
+    IO.write("function call: \n    append(:nil, :nil) \n should result in {:ok, :nil}\n")
+    apply(module, :eval, [seq])
   end
 
   def test(module, 8) do
     seq = [
-      {:match, {:var, :x}, {:cons, {:atm, :a}, {:cons, {:atm, :b}, {:atm, []}}}},
-      {:match, {:var, :y}, {:cons, {:atm, :c}, {:cons, {:atm, :d}, {:atm, []}}}},
-      {:call, :append, [{:var, :x}, {:var, :y}]}
+      {:match, {:var, :x}, {:cons, {:atm, :a}, {:cons, {:atm, :b}, {:atm, :nil}}}},
+      {:match, {:var, :y}, {:cons, {:atm, :c}, {:cons, {:atm, :d}, {:atm, :nil}}}},
+      {:apply, {:fun, :append}, [{:var, :x}, {:var, :y}]}
     ]
-    prgm = prg()
-    IO.write("recursive function: \n   x = [:a, :b]; y = [:c, :d]; append(x, y) \nshould result in {:ok, [:a, :b, :c, :d]}\n")
-    apply(module, :eval, [seq, prgm])
+    IO.write("recursive function: \n   x = {:a, {:b, :nil}}; y = {:c, {:d, :nil}}; append(x, y) \nshould result in {:ok, {:a,{:b,{:c,{:d, :nil}}}}}\n")
+    apply(module, :eval, [seq])
   end
 
   def test(module, 9) do
     seq = [
-      {:match, {:var, :x}, {:cons, {:atm, :a}, {:cons, {:atm, :b}, {:atm, []}}}},
-      {:call, :nreverse, [{:var, :x}]}
+      {:match, {:var, :x}, {:cons, {:atm, :a}, {:cons, {:atm, :b}, {:atm, :nil}}}},
+      {:apply, {:fun, :nreverse}, [{:var, :x}]}
     ]
-    prgm = prg()
-    IO.write("recursive function: \n x = [:a,:b]; nreverse(x)\n should result in {:ok, [:b, :a]}\n")
-    apply(module, :eval, [seq, prgm])
+    IO.write("recursive function: \n x = {:a,{:b,:nil}}; nreverse(x)\n should result in {:ok, {:b, {:a,:nil}}}\n")
+    apply(module, :eval, [seq])
   end
 
   def test(module, 10) do
     seq = [
-      {:match, {:var, :x}, {:cons, {:atm, :a}, {:cons, {:atm, :b}, {:atm, []}}}},
-      {:match, {:var, :y}, {:atm, []}},
+      {:match, {:var, :x}, {:cons, {:atm, :a}, {:cons, {:atm, :b}, {:atm, :nil}}}},
+      {:match, {:var, :y}, {:atm, :nil}},
       {:match, {:var, :f}, {:lambda, [:p], [:y], [{:cons, {:var, :p}, {:var, :y}}]}},
-      {:call, :map, [{:var, :f}, {:var, :x}]}
+      {:apply, {:fun, :map}, [{:var, :f}, {:var, :x}]}
     ]
-    prgm = prg()
-    IO.write("higher order\n   x = [:a, :b]; y = []; f = fn (p) -> [p | y] end; map(f,(x))\n should result in {:ok, [[:a] [:b]]}\n")
-    apply(module, :eval, [seq, prgm])
-  end
-
-  def prg() do
-    [
-      {:nreverse, [:x],
-       [
-         {:case, {:var, :x},
-          [
-            {:clause, {:atm, []}, [{:atm, []}]},
-            {:clause, {:cons, {:var, :hd}, {:var, :tl}},
-             [
-               {:call, :append,
-                [{:call, :nreverse, [{:var, :tl}]}, {:cons, {:var, :hd}, {:atm, []}}]}
-             ]}
-          ]}
-       ]},
-      {:append, [:x, :y],
-       [
-         {:case, {:var, :x},
-          [
-            {:clause, {:atm, []}, [{:var, :y}]},
-            {:clause, {:cons, {:var, :hd}, {:var, :tl}},
-             [{:cons, {:var, :hd}, {:call, :append, [{:var, :tl}, {:var, :y}]}}]}
-          ]}
-       ]},
-      {:map, [:f, :x],
-       [
-         {:case, {:var, :x},
-          [
-            {:clause, {:atm, []}, [{:atm, []}]},
-            {:clause, {:cons, {:var, :hd}, {:var, :tl}},
-             [
-               {:cons, {:apply, {:var, :f}, [{:var, :hd}]},
-                {:call, :map, [{:var, :f}, {:var, :tl}]}}
-             ]}
-          ]}
-       ]}
-    ]
+    IO.write("higher order\n   x = {:a, {:b,:nil}}; y = :nil; f = fn (p) -> {p, y} end; map(f,(x))\n should result in {:ok, {{:a,:nil}, {:b,:nil}}\n")
+    apply(module, :eval, [seq])
   end
 
 end
