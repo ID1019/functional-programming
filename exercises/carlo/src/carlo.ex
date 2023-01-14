2bd28c4e297ce816df902368a8b1a5f22645da78
Johan Montelius <johanmon@kth.se>
2023-01-14 09:20:05 +0100

    update, following the arch

--- exercises/carlo/src/carlo.ex
+++ exercises/carlo/src/carlo.ex
@@ -1,17 +1,17 @@
-defmodule Carlo do
-
+defmodule Carlo do  
+  
   def rounds(k, r)  do
     a = round(1000, r, 0)
     rounds(k, 1000, r, a)
   end
   
-  def rounds(0, t, _, a) do 4*a/t end
-  def rounds(k, t, r, a) do
-    a = round(t, r, a)
-    t = t*2
-    pi = 4*a/t
-    :io.format(" t = ~12w, pi = ~14.10f,  dp = ~14.10f, da = ~8.4f,  dz = ~12.8f\n", [t, pi,  (pi - :math.pi()), (pi - 22/7), (pi - 355/113)])
-    rounds(k-1, t, r, a)
+  def rounds(0, n, _, a) do 4*a/n end
+  def rounds(k, n, r, a) do
+    a = round(n, r, a)
+    n = n*2
+    pi = 4*a/n
+    :io.format(" n = ~12w, pi = ~14.10f,  dp = ~14.10f, da = ~8.4f,  dz = ~12.8f\n", [n, pi,  (pi - :math.pi()), (pi - 22/7), (pi - 355/113)])
+    rounds(k-1, n, r, a)
   end
   
 
@@ -33,5 +33,29 @@ defmodule Carlo do
     :math.pow(r,2) > (:math.pow(x,2) + :math.pow(y,2))
   end
 
+  ## Suming the Leibnitz theorem will rapidly give us a good aproximation.
+  
+  def leibnitz(n) do
+    4 * Enum.reduce(0..n, 0, fn(k,a) -> a + 1/(4*k + 1) - 1/(4*k + 3) end)
+    end
+
+  ## Track the arch satrting in teh upper left corner. In each step we
+  ## move one position in x and probe from the last y value to the next.
+
+  def track(r) do
+    {_, sum} = Enum.reduce(1..r, {r,0}, fn(x, {y, a}) -> y = probe(y,x,r); {y, a+y} end)
+    4 * (sum/:math.pow(r,2))
+  end
+
+  def probe(0, _x, _r) do 0 end
+  def probe(y, x, r) do
+    if :math.pow(r,2) > (:math.pow(x,2) + :math.pow(y,2)) do
+      y
+    else
+      probe(y-1,x,r)
+    end
+  end
+  
+  
 
 end
