a59b5c57ef95c5e155067c61f0b4d4d4ef38ea0e
Johan Montelius <johanmon@kth.se>
2023-02-23 08:44:08 +0100

    updating train shunting

--- exercises/train/src/train.ex
+++ exercises/train/src/train.ex
@@ -5,7 +5,7 @@ defmodule Train do
   def take([h|t], n) when n > 0 do [h|take(t, n-1)] end  
 
 
-  def drop(lst, 0) do lst end
+  def drop(t, 0) do t end
   def drop([_|t], n) when n > 0 do drop(t, n-1) end  
 
   def append([], ys) do ys end
@@ -18,11 +18,21 @@ defmodule Train do
   def position([y|_], y) do 1 end
   def position([_|t], y) do position(t, y) + 1 end  
 
-  def split(train, y) do
-    n = position(train, y)
-    {take(train, n-1), drop(train, n)}
+  def split([y|t], y) do  {[], t}  end
+  def split([h|t], y) do
+    {t, drop} = split(t, y)
+    {[h|t], drop}
   end
-
+  
+  def main([], n) do {n, [], []} end
+  def main([h|t], n) do 
+      case main(t, n) do
+	{0, drop, take} ->
+	  {0, [h|drop], take}
+	{n, drop, take} ->
+	  {n-1, drop, [h|take]}
+      end
+  end
   
 end
 
